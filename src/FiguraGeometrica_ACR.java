862c19099efbdfd03daff1648a825e9fd23ab123
Aaron <text>
2022-02-24 17:45:31 +0100

    versión refactorizada

--- src/FiguraGeometrica_ACR.java
+++ src/FiguraGeometrica_ACR.java
@@ -1,9 +1,9 @@
 
-public abstract class FiguraGeometrica {
+public abstract class FiguraGeometrica_ACR {
 
 	protected String tipoFigura;
 
-	public FiguraGeometrica(String tipoFigura) {
+	public FiguraGeometrica_ACR(String tipoFigura) {
 		super();
 		this.tipoFigura = tipoFigura;
 	}
