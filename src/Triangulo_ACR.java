f33733ae3cf0465c17c70d199a2f80ea96e8196b
Aaron <text>
2022-02-24 17:47:50 +0100

    nuevo método

--- src/Triangulo_ACR.java
+++ src/Triangulo_ACR.java
@@ -23,7 +23,5 @@ public class Triangulo_ACR extends FiguraGeometrica_ACR {
 		return Math.sqrt(sp * (sp-lado1) * (sp-lado2) * (sp-lado3));
 	}
 	
-	public double semiPerimetro() {
-		return this.perimetro() / 2;
-	}
+
 }
