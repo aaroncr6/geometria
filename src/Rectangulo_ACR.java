5cfd8e8ae3ea5b8af8af5efe0010a8f28ee789fb
Aaron <text>
2022-02-24 17:50:25 +0100

    versión comentada

--- src/Rectangulo_ACR.java
+++ src/Rectangulo_ACR.java
@@ -9,11 +9,27 @@ public class Rectangulo_ACR extends FiguraGeometrica_ACR {
 		l2 = lP;
 	}
 
+	
+	/** 
+	 * @author Aarón Cutillas Ruz
+	 * @version 1.2
+	 * calcula el area
+	 * @return double
+	 * devuelve el resultado del area
+	 */
 	@Override
 	public double area() {
 		return l1 * l2;
 	}
 	
+	
+	/**
+	 * @author Aarón Cutillas Ruz
+	 * @version 1.2
+	 * calcula el perimetro 
+	 * @return double
+	 * devuelve el resultado del perimetro
+	 */
 	@Override
 	public double perimetro() {
 		return (2 * l1) + (2 * l2);
